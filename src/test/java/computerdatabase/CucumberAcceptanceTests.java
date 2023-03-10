b09db9bba33b05a7e4d6d537043dcd52cf37d9ff
cauchon211 <cauchon211@C304C-223713.campus.unicaen.fr>
2023-03-10 16:14:56 +0100

    ajout d'un commentaire

--- src/test/java/computerdatabase/CucumberAcceptanceTests.java
+++ src/test/java/computerdatabase/CucumberAcceptanceTests.java
@@ -7,7 +7,7 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-
+// LOL
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("computerdatabase")
